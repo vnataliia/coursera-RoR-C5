a11e305d8e2bda4c6fec5bf4f9768af32d36bfed
vnataliia <nvelyka@gmail.com>
2017-03-28 13:07:32 +0300

    Update routes.js

--- md4/src/routes.js
+++ md4/src/routes.js
@@ -1,9 +1,7 @@
 (function() {
     'use strict';
-
     angular.module('MenuApp').config(RoutesConfig);
     RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
-
     function RoutesConfig ($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.otherwise('/');
 
