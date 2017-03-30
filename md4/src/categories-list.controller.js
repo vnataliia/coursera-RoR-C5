2f54a9913e9fa6a488728488da06944f3e18aea5
vnataliia <nvelyka@gmail.com>
2017-03-30 17:31:48 +0300

    Create categories-list.controller.js

---
+++ md4/src/categories-list.controller.js
@@ -0,0 +1,12 @@
+(function() {
+'use strict';
+
+    angular.module('MenuApp').controller('CategoriesController', CategoriesController);
+
+    CategoriesController.$inject = ['categoriesList'];
+    function CategoriesController (categoriesList) {
+        var ctrl = this;
+        ctrl.categories = categoriesList.data;
+    }
+
+})();
