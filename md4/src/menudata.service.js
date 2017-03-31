5e2f7915863158a5f371468294e8f77f6dc1d9a1
vnataliia <nvelyka@gmail.com>
2017-03-31 14:51:54 +0300

    Update menudata.service.js

--- md4/src/menudata.service.js
+++ md4/src/menudata.service.js
@@ -16,10 +16,12 @@
 			return $http.get(url + 'categories.json').then(function (result) { return result; });
 		};
 	    			console.log('http0 5');
-
+/*
 		service.getItemsForCategory(categoryShortName) = function(){
 			return $http.get(url + 'menu_items.json?category=' + categoryShortName);
-		}
+		}*/
+	    			console.log('http0 e');
+	    			console.log(service);
 	    			console.log('http0 e', service);
 	}
 
