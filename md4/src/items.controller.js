78e629bf26f88598b1dc44e01a575296c75f8037
vnataliia <nvelyka@gmail.com>
2017-03-31 17:25:05 +0300

    Update items.controller.js

--- md4/src/items.controller.js
+++ md4/src/items.controller.js
@@ -6,8 +6,8 @@
   ItemsController.$inject = ['items'];
   function ItemsController(items) {
     var itemsCtrl = this;
-    console.log('ic=', this);
     console.log('items=', items);
+    console.log('ic=', this);
     
 //    itemsCtrl.items = items.menu_items;
   };
