f34c5630fdb5b3713011bd15f692aedeede75b84
vnataliia <nvelyka@gmail.com>
2017-03-31 14:57:48 +0300

    Update menudata.service.js

--- md4/src/menudata.service.js
+++ md4/src/menudata.service.js
@@ -13,12 +13,12 @@
 	    			console.log('http0 2');
 		service.getAllCategories = function(){
 			console.log('http');
-			return $http.get(url + 'categories.json').then(function (result) { return result; });
+			return $http({url: (url + 'categories.json')}).then(function (result) { return result.data; });
 		};
 	    			console.log('http0 5');
 
 		service.getItemsForCategory(categoryShortName) = function(){
-			return $http.get(url + 'menu_items.json?category=' + categoryShortName).then(function (result) { return result; });
+			return $http({url: (url + 'menu_items.json?category=' + categoryShortName)}).then(function (result) { return result.data; });
 		}
 	    			console.log('http0 e');
 	    			console.log(service);
