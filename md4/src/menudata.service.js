2872374b7621a5b04873bd9c724cce0d6b9ce293
vnataliia <nvelyka@gmail.com>
2017-04-05 14:40:21 +0300

    Update menudata.service.js

--- md4/src/menudata.service.js
+++ md4/src/menudata.service.js
@@ -5,27 +5,18 @@
     MenuDataService.$inject = ['$http'];
 	
     function MenuDataService($http) {
-	    			console.log('http0');
-
 		var service = this;
 		var url = 'https://davids-restaurant.herokuapp.com/';
 
-	    			console.log('http0 2');
 		service.getAllCategories = function(){
-			console.log('http');
 			return $http({url: (url + 'categories.json')})
-				.then(function (result) { console.log(result); return result.data; });
+				.then(function (result) { return result.data; });
 		};
-	    			console.log('http0 5');
 	    
 		service.getItemsForCategory = function(categoryShortName) {
-			console.log('http item');
 			return $http({url: (url + 'menu_items.json?category=' + categoryShortName)})
-				.then(function (result) { console.log(result); return result.data; });
-		}
-	    			console.log('http0 e');
-	    			console.log('service = ' + service);
-	    			console.log('http0 e = ' + service);
+				.then(function (result) { return result.data; });
+		};
 	}
 
 })();
