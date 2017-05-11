8f3d96c2aaf5c0b65074798fed2a44bf6968fc45
vnataliia <nvelyka@gmail.com>
2017-05-11 14:30:19 +0300

    Update signup.controller.js

---
+++ md5/src/public/signup/signup.controller.js
@@ -0,0 +1,13 @@
+(function(){
+  'use strict';
+
+  angular.module('public')
+  .controller('SignUpController', SignUpController);
+
+  SignUpController.$inject = ['MenuService'];
+  function SignUpController(MenuService){
+    var $signUpCtrl = this;
+    
+    
+  }
+})();
