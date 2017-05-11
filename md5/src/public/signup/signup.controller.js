fd600353dc6be646773ea1eddf0bba81183d02c6
vnataliia <nvelyka@gmail.com>
2017-05-11 14:36:05 +0300

    Update signup.controller.js

--- md5/src/public/signup/signup.controller.js
+++ md5/src/public/signup/signup.controller.js
@@ -7,7 +7,20 @@
   SignUpController.$inject = ['MenuService'];
   function SignUpController(MenuService){
     var $signUpCtrl = this;
-    
-    
+    $signUpCtrl.user = {};
+    $signUpCtrl.itemFound = true;
+  
+    $signUpCtrl.submit = function () {
+      MenuService.getFavoriteDish($signUpCtrl.user.favoriteDish)
+       .then(function (result) {
+          $signUpCtrl.itemFound = true;
+          $signUpCtrl.user.favoriteDish = result;
+          MenuService.user = $signUpCtrl.user;
+          $signUpCtrl.completed = true;
+        })
+        .catch(function (result) {
+          $signUpCtrl.itemFound = false;
+        });
+    }
   }
 })();
