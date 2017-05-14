d6891d5bd300e8dbcd5d92cb13293bdb31fcfa2a
vnataliia <nvelyka@gmail.com>
2017-05-14 16:06:07 +0300

    Update signup.controller.js

--- md5/src/public/signup/signup.controller.js
+++ md5/src/public/signup/signup.controller.js
@@ -4,18 +4,18 @@
   angular.module('public')
   .controller('SignUpController', SignUpController);
 
-  SignUpController.$inject = ['MenuService'];
-  function SignUpController(MenuService){
+  SignUpController.$inject = ['SignUpService','MenuService'];
+  function SignUpController(SignUpService, MenuService){
     var $signUpCtrl = this;
     $signUpCtrl.user = {};
     $signUpCtrl.itemFound = true;
   
     $signUpCtrl.submit = function () {
-      MenuService.getFavoriteDish($signUpCtrl.user.favoriteDish)
+      SignUpService.getFavoriteDish($signUpCtrl.user.favoriteDish)
        .then(function (result) {
           $signUpCtrl.itemFound = true;
           $signUpCtrl.user.favoriteDish = result;
-          MenuService.user = $signUpCtrl.user;
+          SignUpService.user = $signUpCtrl.user;
           $signUpCtrl.completed = true;
         })
         .catch(function (result) {
