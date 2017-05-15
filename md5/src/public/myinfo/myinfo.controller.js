77710754774bde24e743b1f5e760074f76a77167
vnataliia <nvelyka@gmail.com>
2017-05-15 14:29:29 +0300

    Update myinfo.controller.js

--- md5/src/public/myinfo/myinfo.controller.js
+++ md5/src/public/myinfo/myinfo.controller.js
@@ -7,20 +7,9 @@
   MyInfoController.$inject = ['UserService'];
   function MyInfoController(UserService){
     var $myInfoCtrl = this;
-    $myInfoCtrl.show = true;
-
-    console.log("00 show = ", $myInfoCtrl);
-
     $myInfoCtrl.signed = UserService.signed; 
     $myInfoCtrl.user = UserService.user;
     $myInfoCtrl.fav = UserService.userFav;
-
-    if($myInfoCtrl.user.firstname !== undefined){
-      $myInfoCtrl.show = false;
-    }
-    
-    console.log("99 show = ", $myInfoCtrl);
-
   }
 
 })();
