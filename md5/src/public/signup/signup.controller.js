fe94c7a19161f73f2b3ffc0ea6ea9b52bf914799
vnataliia <nvelyka@gmail.com>
2017-05-15 14:43:29 +0300

    Update signup.controller.js

--- md5/src/public/signup/signup.controller.js
+++ md5/src/public/signup/signup.controller.js
@@ -8,20 +8,20 @@
   function SignUpController(UserService, MenuService){
     var $signUpCtrl = this;
     $signUpCtrl.signed = UserService.signed;
-    $signUpCtrl.itemFound = false;
+    $signUpCtrl.itemNotFound = true;
     $signUpCtrl.user = {};
   
     $signUpCtrl.submit = function () {
       MenuService.getMenuItem($signUpCtrl.user.fav)
        .then(function (result) {
-          $signUpCtrl.itemFound = true;
+          $signUpCtrl.itemNotFound = false;
           UserService.signed = true;
           $signUpCtrl.signed = UserService.signed;
           UserService.user = $signUpCtrl.user;
           UserService.userFav = result;
         })
         .catch(function (result) {
-          $signUpCtrl.itemFound = false;
+          $signUpCtrl.itemNotFound = true;
         });
     }
 
